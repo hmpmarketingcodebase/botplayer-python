ca671f31b6b5082030fe761a90933633f70cca19
zzakariaa10 <zzakariaa10@yahoo.fr>
2018-07-12 00:05:23 +0000

    d

--- shell/windows/by_save.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cd ..
-start python python_files/by_save.py %1 %2 %3 %4 || start python3 python_files/by_save.py %1 %2 %3 %4
--- shell/windows/by_search.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cd ..
-start python python_files/by_search.py %1 %2 %3 %4 || start python3 python_files/by_searchs.py %1 %2 %3 %4
