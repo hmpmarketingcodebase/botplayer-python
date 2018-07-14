5783bb9882d351525173786f4bbb711637755eaa
zzakariaa10 <zzakariaa10@yahoo.fr>
2018-07-14 13:04:32 +0000

    auto pull + auto permission

--- robot.py
+++ robot.py
@@ -8,8 +8,6 @@ if(sys.argv[7]=="windows"):
    cmd=('python robot.py ' + str(sys.argv[1]) + ' ' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) +  ' ' + str(sys.argv[4]) + ' ' + str(sys.argv[5]) + ' ' + str(sys.argv[6]) + ' ' + str(sys.argv[7]) )
    subprocess.call(cmd, shell=True)
 elif(sys.argv[7]=="linux"):
-   os.system("killall python3 chrome chromedriver Xvfb")
-   sleep(2)
    os.system("git stash save --keep-index & disown")
    sleep(5)
    os.system("git pull origin master & disown")
