3bce413bceafb8f16708df7767d335567675261e
zzakariaa10 <zzakariaa10@yahoo.fr>
2018-07-14 13:00:02 +0000

    auto pull + auto permission for linux

--- robot.py
+++ robot.py
@@ -1,15 +1,12 @@
-import math
 import os
 from time import sleep
-import sys
-import MySQLdb
 import datetime
 import subprocess
-sys.path.insert(0, 'python_files/')
-from heart import connectiondb
 
-
-if(opsy=="linux"):
+if(sys.argv[7]=="windows"):         
+   cmd=('python robot.py ' + str(sys.argv[1]) + ' ' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) +  ' ' + str(sys.argv[4]) + ' ' + str(sys.argv[5]) + ' ' + str(sys.argv[6]) + ' ' + str(sys.argv[7]) )
+   subprocess.call(cmd, shell=True)
+elif(sys.argv[7]=="linux"):
    os.system("killall python3 chrome chromedriver Xvfb")
    sleep(2)
    os.system("git stash save --keep-index & disown")
@@ -21,11 +18,6 @@ if(opsy=="linux"):
    sleep(10)
    print("Ready!!")
 
-   
-if(sys.argv[7]=="windows"):         
-         cmd=('python robot.py ' + str(sys.argv[1]) + ' ' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) +  ' ' + str(sys.argv[4]) + ' ' + str(sys.argv[5]) + ' ' + str(sys.argv[6]) + ' ' + str(sys.argv[7]) )
-         subprocess.call(cmd, shell=True)
-elif(sys.argv[7]=="linux"):
-         cmd=('python3 robot.py ' + str(sys.argv[1]) + ' ' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) +  ' ' + str(sys.argv[4]) + ' ' + str(sys.argv[5]) + ' ' + str(sys.argv[6]) + ' ' + str(sys.argv[7]))
-         subprocess.call(cmd, shell=True)
+   cmd=('python3 robot.py ' + str(sys.argv[1]) + ' ' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) +  ' ' + str(sys.argv[4]) + ' ' + str(sys.argv[5]) + ' ' + str(sys.argv[6]) + ' ' + str(sys.argv[7]))
+   subprocess.call(cmd, shell=True)
    
