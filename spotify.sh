daa7f15a0a450cd3e760419f7425c540921b3983
zzakariaa10 <zzakariaa10@yahoo.fr>
2018-08-30 00:24:05 +0000

    sh script

--- spotify.sh
+++ spotify.sh
@@ -1,6 +1,6 @@
 while true
 do
-   python3 spotify.py spot_test4 $1 $2 $3 $4 & disown
+   python3 spotify.py $1 $2 $3 $4 & disown
    sleep 25s
    killall python3 Xvfb chrome chromedriver
    sleep 6s
