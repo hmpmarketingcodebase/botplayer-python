ecea5a5156572863ef3a32d26a12431b79a6b11f
Zak <zzakariaa10@yahoo.fr>
2018-12-15 12:02:02 +0000

    --

--- scriptes/common/loop.sh
+++ scriptes/common/loop.sh
@@ -1,7 +1,13 @@
+depth=0;
 while true
 do
    sudo bash clean_tmp.sh
    sleep 1600s
    sudo bash clean_ram.sh
    sleep 1600s
+   
+   let "depth++"
+   if [ `expr $depth % 10` -eq 0 ]; then
+   sudo killall chrome chromedriver
+   fi
 done
