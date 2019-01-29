386fd448280be239a858405aeba49d3293a8a700
Zak <zzakariaa10@yahoo.fr>
2019-01-29 11:08:45 +0000

    -

---
+++ scriptes/common/manager2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+t=0
+while true
+do
+nohup sudo killall python3 chrome chromedriver Xvfb 0</dev/null &
+
+echo "Clean DISK"
+nohup sudo bash clean_tmp.sh 0</dev/null &
+sleep(5)
+echo "clean RAM"
+nohup sudo bash clean_ram.sh 0</dev/null &
+
+sleep 30s
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+nohup python3 ../spotify/$1 0</dev/null &
+sleep 8000s
+
+done
