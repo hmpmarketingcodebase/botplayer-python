f4106f9d1a39a417cf6326163cce7d3d521ed3a7
Zak <zzakariaa10@yahoo.fr>
2019-02-02 13:29:37 +0000

    t

--- scriptes/common/loop.py
+++ scriptes/common/loop.py
@@ -1,9 +1,10 @@
 import os
+from time import sleep
 while(1):
-    os.system('sudo bash clean_tmp.sh & disown')
+    os.system('nohup sudo bash clean_tmp.sh 0</dev/null &')
     print("clear tmp file")
     sleep(5)
     print("clear cache RAM") 
-    os.system('sudo bash clean_ram.sh & disown')
+    os.system('nohup sudo bash clean_ram.sh 0</dev/null &')
     sleep(5)
-    os.system('sudo killall chrome chromedriver')
+    os.system('nohup sudo killall chrome chromedriver 0</dev/null &')
